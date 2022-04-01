c8aecd45ec66af2aa61074ee1ab48de14bcfe30e
vaida-r <vreinartaite@gmail.com>
2022-04-01 18:12:18 +0300

    Day 1 Task1 commit

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -1,12 +1,20 @@
 package selenium.tasks;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.util.Locale;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class Task1 {
     WebDriver driver;
@@ -15,7 +23,9 @@ public class Task1 {
     public void openPage() {
 
         String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        //?????????????? - dar perziureti kaip sudda sita properti
+        //System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Vaida\\Desktop\\bselenium\\selenium_java_basic\\lib\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
     }
@@ -26,15 +36,30 @@ public class Task1 {
     }
 
     @Test
-    public void errorOnText() {
+    public void errorOnText() throws InterruptedException {
 //        TODO
+        String enterText = "kazkas";
 //        enter a text instead of a number, check that correct error is seen
+        WebElement number = driver.findElement(By.id("numb"));
+        number.sendKeys(enterText);
+        driver.findElement(By.tagName("button")).click();
+        String errMessage = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals("Please enter a number", errMessage );
+
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        String enterText = "40";
+//        enter a text instead of a number, check that correct error is seen
+        WebElement number = driver.findElement(By.id("numb"));
+        number.sendKeys(enterText);
+        driver.findElement(By.tagName("button")).click();
+        String errMessage = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals("Number is too small", errMessage );
+
     }
 
     @Test
@@ -43,6 +68,14 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        String enterText = "250";
+//        enter a text instead of a number, check that correct error is seen
+        WebElement number = driver.findElement(By.id("numb"));
+        number.sendKeys(enterText);
+        driver.findElement(By.tagName("button")).click();
+        String errMessage = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals("Number is too big", errMessage );
+
     }
 
     @Test
@@ -50,12 +83,75 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        String enterText = "64";
+        int testNumber = 64;
+//        enter a text instead of a number, check that correct error is seen
+
+        WebElement number = driver.findElement(By.id("numb"));
+        number.sendKeys(enterText);
+        driver.findElement(By.tagName("button")).click();
+
+        String expMessage = String.format(Locale.US, "Square root of %d is %.2f",testNumber,Math.sqrt(testNumber));
+
+        Alert alert = driver.switchTo().alert();
+        String actMessage = alert.getText();
+        alert.accept();
+
+        assertEquals(expMessage, actMessage );
+
     }
+
+
 
     @Test
     public void correctSquareRootWithRemainder() {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        String enterText = "70";
+        int testNumber = 70;
+//        enter a text instead of a number, check that correct error is seen
+
+        WebElement number = driver.findElement(By.id("numb"));
+        number.sendKeys(enterText);
+        driver.findElement(By.tagName("button")).click();
+
+        String expMessage = String.format(Locale.US, "Square root of %d is %.2f",testNumber,Math.sqrt(testNumber));
+
+        Alert alert = driver.switchTo().alert();
+        String actMessage = alert.getText();
+        alert.accept();
+
+        assertEquals(expMessage, actMessage );
+
     }
+
+    @Test
+    public void correctSquareRootForAllAllowedNumber() {       // Sergey's suggestion
+//        TODO
+//        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
+//        then check that correct no error is seen and check that square root is calculated correctly
+        for (int i =50; i<=100; i++) {
+
+            int testNumber = i;
+
+            WebElement number = driver.findElement(By.id("numb"));
+            number.sendKeys(String.valueOf(testNumber));
+            driver.findElement(By.tagName("button")).click();
+
+            String expMessage = String.format(Locale.US, "Square root of %d is %.2f", testNumber, Math.sqrt(testNumber));
+
+            Alert alert = driver.switchTo().alert();
+            String actMessage = alert.getText();
+            alert.accept();
+
+            assertEquals(expMessage, actMessage);
+
+        }
+    }
+
+
+
+
+
 }
